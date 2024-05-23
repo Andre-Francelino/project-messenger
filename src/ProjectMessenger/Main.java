45d0f1dcfb30f21f749b0190b4762d46be1e5d5f
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-05-22 21:11:10 -0300

    Welcome to project messenger

---
+++ src/ProjectMessenger/Main.java
@@ -0,0 +1,9 @@
+package ProjectMessenger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Bem Vindo ao projeto Messenger");
+        System.out.println("Estudando paradigmas da Programação Orientada a Objetos!");
+    }
+}
