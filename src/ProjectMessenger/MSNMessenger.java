0448d5eb9d777c7663caf92c7e307a7bdb78e333
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-05-22 21:21:03 -0300

    MSN

---
+++ src/ProjectMessenger/MSNMessenger.java
@@ -0,0 +1,21 @@
+package ProjectMessenger;
+
+public class MSNMessenger {
+
+    public void enviarMensagem() {
+        System.out.println("Enviando Mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo Mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado à internet");
+    }
+
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando o histórico da Mensagem");
+    }
+
+}
