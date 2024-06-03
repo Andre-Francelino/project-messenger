abc3c7d487e53f04fac995f361394715a60a0dc3
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-06-02 21:08:56 -0300

    Abstração

--- src/ProjectMessenger/FacebookMessenger.java
+++ src/ProjectMessenger/FacebookMessenger.java
@@ -2,4 +2,13 @@ package ProjectMessenger;
 
 public class FacebookMessenger extends ServicoMensagemInstantanea {
 
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook !");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook ! \n");
+    }
 }
--- src/ProjectMessenger/ServicoMensagemInstantanea.java
+++ src/ProjectMessenger/ServicoMensagemInstantanea.java
@@ -1,26 +1,8 @@
 package ProjectMessenger;
 
-public class ServicoMensagemInstantanea {
+public abstract class ServicoMensagemInstantanea {
 
-    public void enviarMensagem() {
-        // confirmando se o usuário está conectado à internet
-        validarConectadoInternet();
-        System.out.println("Enviando Mensagem");
-        // após o envio a mensagem é salva no histórico
-        salvarHistoricoMensagem();
-    }
-
-    public void receberMensagem() {
-        System.out.println("Recebendo Mensagem \n");
-    }
-
-    // métodos privados, visiveis somente na classe
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado à internet");
-    }
-
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da Mensagem");
-    }
+    public abstract void enviarMensagem();
+    public abstract void receberMensagem();
 
 }
